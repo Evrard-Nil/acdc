cdbecef65b1795045edea3774c779f8f6b8b40b4
evrard-nil <evrardnild@gmail.com>
2018-10-21 16:10:20 +0200

    Implemented process killing of site demo and git commit, still TODO: git push

--- code/src/V2/Main.java
+++ code/src/V2/Main.java
@@ -1,68 +1,65 @@
 package V2;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.Executors;
 
 
 
 public class Main {
-	
-	
+
+
 
 	public static void main(String[] args) {
-		
-		
-		
+
+
+
 		// TODO Auto-generated method stub
 		Post t = new Post();
-		
-//		Scanner sc=new Scanner(System.in);
-//		Scanner scInt=new Scanner(System.in);
-//		
-//		System.out.println("Indiquer le titre de la publication\n");
-//		t.setTitle(sc.nextLine());
-//		System.out.println("Indiquer la date de la publication (YYY-MM-DD):\n");
-//		t.setDate(sc.nextLine());
-//		System.out.println("Indiquer l'autheur de la publication\n");
-//		t.setAuthor(sc.nextLine());
-//		System.out.println("Quelle type de publication ?\n");
-//		List<String> s = t.getAllCat();
-//		System.out.println("0. - Créer nouvelle categorie\n");
-//		for(int i=0;i<t.getAllCat().size(); i++) {
-//			System.out.println(Integer.toString(i+1)+". - "+s.get(i));
-//		}
-//		int r=scInt.nextInt();
-//		if(r == 0) {
-//			System.out.println("Indiquer le nom souhaité de la nouvelle catégorie \n");
-//			String newCat =sc.nextLine();
-//			t.addCateg(newCat);
-//			t.setCategory(newCat);
-//		}
-//		else {
-//			t.setCategory(s.get(r-1));
-//		}
-//		
-//		System.out.println("texte de la publication");
-//		t.setContent(sc.nextLine());
-//		
-//		sc.close();
-//		scInt.close();
-//		
-//		System.out.println(t.toMarkdown());
-//		t.writeFile(t.toMarkdown());
-//		
+
+		Scanner sc=new Scanner(System.in);
+		Scanner scInt=new Scanner(System.in);
+
+		System.out.println("Indiquer le titre de la publication\n");
+		t.setTitle(sc.nextLine());
+		System.out.println("Indiquer la date de la publication (YYY-MM-DD):\n");
+		t.setDate(sc.nextLine());
+		System.out.println("Indiquer l'autheur de la publication\n");
+		t.setAuthor(sc.nextLine());
+		System.out.println("Quelle type de publication ?\n");
+		List<String> s = t.getAllCat();
+		System.out.println("0. - Créer nouvelle categorie\n");
+		for(int i=0;i<t.getAllCat().size(); i++) {
+			System.out.println(Integer.toString(i+1)+". - "+s.get(i));
+		}
+		int r=scInt.nextInt();
+		if(r == 0) {
+			System.out.println("Indiquer le nom souhaité de la nouvelle catégorie \n");
+			String newCat =sc.nextLine();
+			t.addCateg(newCat);
+			t.setCategory(newCat);
+		}
+		else {
+			t.setCategory(s.get(r-1));
+		}
+
+		System.out.println("texte de la publication");
+		t.setContent(sc.nextLine());
+
+
+		System.out.println(t.toMarkdown());
+		t.writeFile(t.toMarkdown());
+
 
 		try {
 			t.demo();
-			t.git();
+			t.git("add of file: "+t.getDate()+"-"+t.getTitle().replace(' ','_')+".markdown to _testsite folder");
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-				
+		
+		sc.close();
+		scInt.close();
+
 	}
 
 
--- code/src/V2/Post.java
+++ code/src/V2/Post.java
@@ -6,24 +6,35 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-
+/**
+ * 
+ * @author Evrard-Nil Daillet
+ * 
+ * @version 3
+ * 
+ * This class is an implementation of a publication dynamically created and added to a static website.
+ * 
+ *
+ */
 public class Post {
-	
+
 	private String title;
 	private String date;
-	private String content;
+	private String content;	
 	private String author;
 	private String category;
 	private String layout="post";
-	private String path=".."+File.separator+ "_testsite"+File.separator+ "_posts"+File.separator;
-	
+	private String path="/home/daillet/IMT/project"+File.separator+ "_testsite"+File.separator+ "_posts"+File.separator;
+
 	public Post() {
-		
 	}
 
 	public String getTitle() {
@@ -73,7 +84,7 @@ public class Post {
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
+
 	public String getLayout() {
 		return layout;
 	}
@@ -84,127 +95,129 @@ public class Post {
 
 	public String toMarkdown() {
 		return "---\n"
-				+"layout:"+this.layout+ "\n"
-				+"title:"+this.title+"\n"
-				+"categories:"+this.category+"\n"
-				+"date:"+this.date+"\n"
+				+"layout: "+this.layout+ "\n"
+				+"title: \""+this.title+"\"\n"
+				+"categories: "+this.category+"\n"
+				+"date: "+this.date+"\n"
 				+"---\n\n"
 				+"*"+this.author+"*\n\n"
 				+this.content
 				;
 	}
-	
+
 	public void writeFile(String contentAdd) {
 		// Write file 
-		String filename= date+"-"+this.title.replace(' ','_')+".markdown";
-		BufferedWriter writer;
+		String filename= this.path + this.date+"-"+this.title.replace(' ','_')+".markdown";
+		PrintWriter writer;
 		try {
-			writer = new BufferedWriter(new FileWriter(filename));
-			writer.write(contentAdd);
+			writer = new PrintWriter(filename);
+			writer.print(contentAdd);
 			writer.close();		
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	
+
 	}
-	
+
 	/**
 	 * Open and read a file, and return the lines in the file as a list
 	 * of Strings.
 	 * (Demonstrates Java FileReader, BufferedReader, and Java5.)
 	 */
 	public List<String> getAllCat() {
-	  List<String> records = new ArrayList<String>();
-	  try
-	  {
-	    BufferedReader reader = new BufferedReader(new FileReader("categories"));
-	    String line;
-	    while ((line = reader.readLine()) != null)
-	    {
-	    	records.add(line);
-	    }
-	    reader.close();
-	    return records;
-	  }
-	  catch (Exception e)
-	  {
-	    System.err.format("Exception occurred trying to read '%s'.", "categories");
-	    e.printStackTrace();
-	    return null;
-	  }
+		List<String> records = new ArrayList<String>();
+		try
+		{
+			BufferedReader reader = new BufferedReader(new FileReader("categories"));
+			String line;
+			while ((line = reader.readLine()) != null)
+			{
+				records.add(line);
+			}
+			reader.close();
+			return records;
+		}
+		catch (Exception e)
+		{
+			System.err.format("Exception occurred trying to read '%s'.", "categories");
+			e.printStackTrace();
+			return null;
+		}
 	}
-	
+
 	public void addCateg(String newCat) {
 		try
-		  {
-		    BufferedWriter w = new BufferedWriter(new FileWriter("categories", true));
-		    w.append(newCat+"\n");
-		    w.close();
-		  }
-		  catch (Exception e)
-		  {
-		    System.err.format("Exception occurred trying to write '%s'.", "categories");
-		    e.printStackTrace();
-		  }
+		{
+			BufferedWriter w = new BufferedWriter(new FileWriter("categories", true));
+			w.append(newCat+"\n");
+			w.close();
+		}
+		catch (Exception e)
+		{
+			System.err.format("Exception occurred trying to write '%s'.", "categories");
+			e.printStackTrace();
+		}
 	}
-	// TODO: VOID LAUNCH 		
+	 		
 	private static class StreamGobbler implements Runnable {
-        private java.io.InputStream inputStream;
-        private java.util.function.Consumer<String> consumer;
-     
-        public StreamGobbler(java.io.InputStream inputStream, java.util.function.Consumer<String> consumer) {
-            this.inputStream = inputStream;
-            this.consumer = consumer;
-        }
-     
-        @Override
-        public void run() {
-            new java.io.BufferedReader(new java.io.InputStreamReader(inputStream)).lines()
-              .forEach(consumer);
-        }
-    }
-    
-    public void demo() throws InterruptedException {
-    	final boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
-    	try {
-    		ProcessBuilder builder = new ProcessBuilder();
-    	if (isWindows) {
-    	    builder.command("cmd.exe", "/c", "bundle exec jekyll serve -o");
-    	} else {
-    	    builder.command("sh", "-c", "bundle exec jekyll serve -o");
-    	}
-    	builder.directory(new File(System.getProperty("user.home")+"/IMT/project/_testsite/"));
-    	Process process = builder.start();
-    	StreamGobbler streamGobbler = 
-    	  new StreamGobbler(process.getInputStream(), System.out::println);
-    	Executors.newSingleThreadExecutor().submit(streamGobbler);
-    	int exitCode = process.waitFor();
-    	assert exitCode == 0;
-    	} catch (IOException e) {
-    		// TODO Auto-generated catch block
-    		e.printStackTrace();
-    	}
+		private java.io.InputStream inputStream;
+		private java.util.function.Consumer<String> consumer;
+
+		public StreamGobbler(java.io.InputStream inputStream, java.util.function.Consumer<String> consumer) {
+			this.inputStream = inputStream;
+			this.consumer = consumer;
+		}
+
+		@Override
+		public void run() {
+			new java.io.BufferedReader(new java.io.InputStreamReader(inputStream)).lines()
+			.forEach(consumer);
+		}
 	}
-	// TODO: PUSHGIT()
-    public void git() throws InterruptedException {
-    	final boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
-    	try {
-    		ProcessBuilder builder = new ProcessBuilder();
-    	if (isWindows) {
-    	    builder.command("cmd.exe", "/c", "bundle exec jekyll serve -o");
-    	} else {
-    	    builder.command("sh", "-c", "git pull", "git add .", "git commit", "git push", "evrard-nil", "Ansaerys123");
-    	}
-    	builder.directory(new File(System.getProperty("user.home")+"/IMT/project/_testsite/"));
-    	Process process = builder.start();
-    	StreamGobbler streamGobbler = 
-    	  new StreamGobbler(process.getInputStream(), System.out::println);
-    	Executors.newSingleThreadExecutor().submit(streamGobbler);
-    	int exitCode = process.waitFor();
-    	assert exitCode == 0;
-    	} catch (IOException e) {
-    		// TODO Auto-generated catch block
-    		e.printStackTrace();
-    	}	
+
+	public void demo() throws InterruptedException {
+		final boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+		try {
+			ProcessBuilder builder = new ProcessBuilder();
+			if (isWindows) {
+				builder.command("cmd.exe", "/c", "bundle exec jekyll serve");
+			} else {
+				builder.command("sh", "-c", "bundle exec jekyll serve -o");
+			}
+			builder.directory(new File(System.getProperty("user.home")+"/IMT/project/_testsite/"));
+			Process process = builder.start();
+			StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+			Executors.newSingleThreadExecutor().submit(streamGobbler);
+			TimeUnit.SECONDS.sleep(3);
+			System.out.println("Press enter to stop demo");
+			System.in.read();
+			System.out.println("Demo killed");
+			process.destroy();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public void git(String commit) throws InterruptedException {
+		final boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+		try {
+			ProcessBuilder builder = new ProcessBuilder();
+			if (isWindows) {
+				builder.command("cmd.exe", "/c", "git status");
+			} else {
+				builder.command("sh", "-c", "git status ; git add . ; git commit -m \""+commit+"\"; git status ; git push");
+			}
+			builder.directory(new File(System.getProperty("user.home")+"/IMT/project/_testsite/"));
+			Process process = builder.start();
+			StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+			Executors.newSingleThreadExecutor().submit(streamGobbler);
+			int exitCode = process.waitFor();
+			assert exitCode == 0;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
